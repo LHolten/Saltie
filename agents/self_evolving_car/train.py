3b7cba1e14154d4a10ad59929abf5bda85bcc8c4
Hytak <lhc.holten@gmail.com>
2019-01-27 15:21:24 +0100

    revert mutation remove crossover add early stopping of attempts add fitness for ball touch square fitness restructure

--- agents/self_evolving_car/train.py
+++ agents/self_evolving_car/train.py
@@ -19,24 +19,32 @@ class PythonExample(BaseAgent):
         self.Model = SymmetricModel
         self.torch = torch
         self.controller_state = SimpleControllerState()
+
         self.frame = 0  # frame counter for timed reset
+        self.attempt = 0
         self.brain = 0  # bot counter for generation reset
-        self.pop = 10  # population for bot looping
         self.gen = 0
-        self.min_distance_to_ball = []
+
         self.ball_set = ((0, 1000, 0), (0, 0, 0)), ((1000, 0, 1000), (0, 0, 0)), \
                         ((-1000, 0, 1000), (0, 0, 0)), ((-2000, -2000, 1000), (0, 0, 0)), \
                         ((2000, -2000, 1000), (0, 0, 0))
-        self.attempt = 0
+
         self.max_frames = 5000
+        self.num_attempts = len(self.ball_set)
+        self.pop = 10  # population for bot looping
+
         self.bot_list = [self.Model() for _ in range(self.pop)]  # list of Individual() objects
-        self.bot_fitness = [math.inf] * self.pop
         self.parent = [0, 1]  # fittest object
+
         self.mut_rate = 1  # mutation rate
         self.mut_multiplier = 0.8  # decreasing this will make the mutation rate increase and decrease faster
         self.mut_power = 3  # increasing this will make the mutation rate decrease faster
         self.mut_min = 0.01  # mutation rate will not go below this value
+
         self.distance_to_ball = [math.inf] * self.max_frames  # set high for easy minimum
+        self.min_distance_to_ball = [0] * self.num_attempts  # set 0 for easy current total
+        self.bot_fitness = [math.inf] * self.pop  # set high for easy minimum
+
         self.input_formatter = LeviInputFormatter(team, index)
         self.output_formatter = LeviOutputFormatter(index)
 
@@ -52,17 +60,14 @@ class PythonExample(BaseAgent):
         distance_to_ball_x = packet.game_ball.physics.location.x - my_car.physics.location.x
         distance_to_ball_y = packet.game_ball.physics.location.y - my_car.physics.location.y
         distance_to_ball_z = packet.game_ball.physics.location.z - my_car.physics.location.z
-        self.distance_to_ball[self.frame] = math.sqrt(
-            distance_to_ball_x ** 2 + distance_to_ball_y ** 2 + distance_to_ball_z ** 2)
+        self.distance_to_ball[self.frame] = distance_to_ball_x ** 2 + distance_to_ball_y ** 2 + distance_to_ball_z ** 2
+
+        if packet.game_ball.latest_touch.time_seconds > packet.game_info.seconds_elapsed - 0.02:
+            self.distance_to_ball[self.frame] = 0
 
         # RENDER RESULTS
         action_display = "GEN: " + str(self.gen + 1) + " | BOT: " + str(self.brain + 1)
-
         draw_debug(self.renderer, action_display)
-
-        # STOP EVOLVING WHEN THE BALL IS TOUCHED
-        # if packet.game_ball.latest_touch.player_name == "Self-Evolving-Car":
-        #  self.mut_rate = 0
 
         # GAME STATE
         car_state = CarState(boost_amount=100)
@@ -82,7 +87,7 @@ class PythonExample(BaseAgent):
         stop_attempt = self.frame > 50 and \
                        (my_car.physics.location.z < 100 or my_car.physics.location.z > 1950 or
                         my_car.physics.location.x < -4000 or my_car.physics.location.x > 4000 or
-                        my_car.physics.location.y > 5000 or my_car.physics.location.y < -5000 or
+                        my_car.physics.location.y > 2000 or my_car.physics.location.y < -5000 or
                         self.distance_to_ball[self.frame] > self.distance_to_ball[self.frame - 1])
 
         # LOOPS
@@ -94,21 +99,27 @@ class PythonExample(BaseAgent):
             self.calc_fitness()
             self.calc_fittest()
 
-            stop_brain = max(self.parent) != self.brain and self.brain > 1
+            stop_brain = (max(self.parent) != self.brain) and self.brain >= 2
 
             self.attempt += 1
-            if self.attempt >= 5 or stop_brain:
+            if self.attempt >= self.num_attempts or stop_brain:
                 self.attempt = 0
 
-                self.min_distance_to_ball = []
+                self.min_distance_to_ball = [0] * self.num_attempts  # set 0 for easy current total
 
                 self.brain += 1  # change bot every reset
                 if self.brain >= self.pop:
                     self.brain = 0  # reset bots after all have gone
 
-                    self.next_generation()
+                    self.adaptive_mut_rate()
 
-                    self.bot_fitness = [math.inf] * self.pop
+                    self.print_generation_info()
+
+                    # NE Functions
+                    self.selection()
+                    self.mutate()
+
+                    self.bot_fitness = [math.inf] * self.pop  # reset bot fitness
 
                     self.gen += 1
 
@@ -119,33 +130,27 @@ class PythonExample(BaseAgent):
 
     def calc_min_fitness(self):
         # CALCULATE MINIMUM DISTANCE TO BALL FOR EACH ATTEMPT
-        self.min_distance_to_ball.append(min(self.distance_to_ball))
+        self.min_distance_to_ball[self.attempt] = min(self.distance_to_ball)
         self.distance_to_ball = [math.inf] * self.max_frames
 
     def calc_fitness(self):
         # CALCULATE AVERAGE OF MINIMUM DISTANCE TO BALL FOR EACH GENOME
-        sum1 = sum(self.min_distance_to_ball)
+        total = sum(self.min_distance_to_ball)
         # sum1 /= len(self.min_distance_to_ball)
 
-        self.bot_fitness[self.brain] = sum1
+        self.bot_fitness[self.brain] = total
 
-    def next_generation(self):
-        self.adaptive_mut_rate()
-
+    def print_generation_info(self):
         # PRINT GENERATION INFO
         print("")
         print("     GEN = " + str(self.gen + 1))
         print("-------------------------")
         print("FITTEST = BOT " + str(self.parent[0] + 1))
-        print("------FITNESS = " + str(self.bot_fitness[self.parent[0]]))
+        print("------FITNESS = " + str(math.sqrt(self.bot_fitness[self.parent[0]] / 5)))
         # print("------WEIGHTS = " + str(self.bot_list[self.fittest]))
         for i in range(len(self.bot_list)):
-            print("FITNESS OF BOT " + str(i + 1) + " = " + str(self.bot_fitness[i]))
+            print("FITNESS OF BOT " + str(i + 1) + " = " + str(math.sqrt(self.bot_fitness[i] / 5)))
         print("------MUTATION RATE = " + str(self.mut_rate))
-
-        # NE Functions
-        self.selection()
-        self.mutate()
 
     def calc_fittest(self):
         temp = [math.inf] * 2
@@ -159,7 +164,7 @@ class PythonExample(BaseAgent):
                 self.parent[1], self.parent[0] = self.parent[0], self.parent[1]
 
     def adaptive_mut_rate(self):
-        if min(self.parent) >= 2:
+        if self.parent[0] >= 2:
             # new better bot!
             self.mut_rate /= self.mut_multiplier
         else:
@@ -181,24 +186,29 @@ class PythonExample(BaseAgent):
     def selection(self):
         # COPY FITTEST WEIGHTS TO FIRST TWO BOTS
         fittest_dicts = [self.bot_list[parent_index].state_dict() for parent_index in self.parent]
-        for index in range(2):
-            self.bot_list[index].load_state_dict(fittest_dicts[index])
-
-        # CROSSOVER TO CREATE THE OTHER BOTS
-        for bot in self.bot_list[2:]:
-            for param, param_parent1, param_parent2 in zip(bot.parameters(),
-                                                           self.bot_list[0].parameters(),
-                                                           self.bot_list[1].parameters()):
-                mask = self.torch.rand(param.data.size()) * (1 - min(self.mut_rate * 10, 1))
-                param.data = (param_parent1.data * (1 - mask) + param_parent2.data * mask).clone()
+        # for index in range(2):
+        #     self.bot_list[index].load_state_dict(fittest_dicts[index])
+        #
+        # # CROSSOVER TO CREATE THE OTHER BOTS
+        # for bot in self.bot_list[2:]:
+        #     for param, param_parent1, param_parent2 in zip(bot.parameters(),
+        #                                                    self.bot_list[0].parameters(),
+        #                                                    self.bot_list[1].parameters()):
+        #         mask = self.torch.rand(param.data.size()) * (1 - min(self.mut_rate * 10, 1))
+        #         param.data = (param_parent1.data * (1 - mask) + param_parent2.data * mask).clone()
+        for index in range(self.pop):
+            self.bot_list[index].load_state_dict(fittest_dicts[index % 2])
 
     def mutate(self):
         # MUTATE NEW GENOMES
         for bot in self.bot_list[2:]:
             new_genes = self.Model()
             for param, param_new in zip(bot.parameters(), new_genes.parameters()):
-                mask = self.torch.rand(param.data.size()) * min(self.mut_rate, 1)
-                param.data = (param.data * (1 - mask) + param_new.data * mask).clone()
+                # mask = (self.mut_rate / self.torch.rand(param.data.size())).clamp(0, 1)
+                mask = self.torch.rand(param.data.size()) < self.mut_rate
+                # param.data = (param.data * (1 - mask) + param_new.data * mask).clone()
+                param.data[~mask] += (param_new.data[~mask] * self.mut_rate).clone()
+                param.data[mask] = param_new.data[mask].clone()
 
 
 def draw_debug(renderer, action_display):
